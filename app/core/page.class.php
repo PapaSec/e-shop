19016c8451a1a059667ca861e8dbe9138f6b146d
PapaSec <fransronald@gmail.com>
2025-03-27 21:32:30 +0200

    Daily Update

---
+++ app/core/page.class.php
@@ -0,0 +1,30 @@
+<?php
+
+class Page
+{
+
+    public static function links()
+    {
+        $links = (object)[];
+
+        $links->prev = "";
+        $links->next = "";
+        $query_string = str_replace("url=", "", $_SERVER['QUERY_STRING']);
+
+        $page_number = isset($_GET['pg']) ? (int)$_GET['pg'] : 1;
+        $page_number = $page_number < 1 ? 1 : $page_number;
+
+        $next_page = $page_number + 1;
+        $prev_page = ($page_number > 1) ? $page_number - 1 : 1;
+
+        $current_link = ROOT . $query_string;
+        if (!strstr($current_link, "pg=")) {
+            $current_link .= "&pg=1";
+        }
+        $links->prev = preg_replace("/pg=[^&?=]+/", "pg=" . $prev_page, $current_link);
+        $links->next = preg_replace("/pg=[^&?=]+/", "pg=" . $next_page, $current_link);
+        $links->current = $page_number;
+
+        return $links;
+    }
+}
--- app/views/eshop/shop.php
+++ app/views/eshop/shop.php
@@ -28,14 +28,14 @@
 
 					<br style="clear: both;">
 					<ul class="pagination">
-						<li><a href="<?= $page_links->prev ?>">Prev</a></li>
+						<li><a href="<?= Page::links()->prev ?>">Prev</a></li>
 
-						<li <?= ($page_links->current == 1) ? 'class="active"' : ''; ?>><a href="">1</a></li>
-						<li <?= ($page_links->current == 2) ? 'class="active"' : ''; ?>><a href="">2</a></li>
-						<li <?= ($page_links->current == 3) ? 'class="active"' : ''; ?>><a href="">3</a></li>
+						<li <?= (Page::links()->current == 1) ? 'class="active"' : ''; ?>><a href="">1</a></li>
+						<li <?= (Page::links()->current == 2) ? 'class="active"' : ''; ?>><a href="">2</a></li>
+						<li <?= (Page::links()->current == 3) ? 'class="active"' : ''; ?>><a href="">3</a></li>
 						<li><a href="">&raquo;</a></li>
 
-						<li><a href="<?= $page_links->next ?>">Next</a></li>
+						<li><a href="<?= Page::links()->next ?>">Next</a></li>
 					</ul>
 				</div><!--features_items-->
 			</div>
